f4d247491ae671b62128aabe529bf97989782552
AlexVeliksar <alex.veliksar@gmail.com>
2025-09-16 14:42:06 +0300

    Added plugs in place of matrix operations

--- src/calculation.cpp
+++ src/calculation.cpp
@@ -7,27 +7,27 @@ void calculation_matrix(int operation) {
     {
     // нахождение det
     case 1:
-        
+        std::cout << "In process" << std::endl;
         break;
     // сложение матриц
     case 2:
-        
+        std::cout << "In process" << std::endl;
         break;
     // вычитание матриц
     case 3:
-        
+        std::cout << "In process" << std::endl;
         break;
     // умножение матриц
     case 4:
-
+        std::cout << "In process" << std::endl;
         break;
     // находение обратной матрицы
     case 5:
-
+        std::cout << "In process" << std::endl;
         break;
     // транспонирование матрицы
     case 6:
-        
+        std::cout << "In process" << std::endl;
         break;
     default:
         break;
